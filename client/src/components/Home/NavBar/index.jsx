fb13e19562345a9b9c5e90d78ea666bad7aaa9ef
Sebastian Coronel <scoronelhamilton@gmail.com>
2020-03-08 13:51:21 -0400

    center elements in navbar

--- client/src/components/Home/NavBar/index.jsx
+++ client/src/components/Home/NavBar/index.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 const NavBar = ({ setShowPortfolio }) => {
   return (
     <div id="navbar">
-      <a onClick={() => setShowPortfolio(true)}>Portfolio</a>
-      <a onClick={() => setShowPortfolio(false)}>Transactions</a>
+      <div className="navbar-wrapper">
+        <a>Home</a>
+        <div>
+          <a onClick={() => setShowPortfolio(true)}>Portfolio</a>
+          <a onClick={() => setShowPortfolio(false)}>Transactions</a>
+        </div>
+      </div>
     </div>
   );
 };
