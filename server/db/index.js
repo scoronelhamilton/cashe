4c7ed862a3e6255418c79a7bfe237f3ea7d3da91
Sebastian Coronel <scoronelhamilton@gmail.com>
2020-03-08 19:49:50 -0400

    add mongoDB URI to env file

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
-const host = process.env.MONGOLAB_URI || 'mongodb://localhost/cashe';
+const host = process.env.MONGODB_URI || 'mongodb://localhost/cashe';
 
 mongoose
   .connect(host, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => {
+    console.log(host);
+    console.log('Connected to MongoDB');
+  })
   .catch(err => {
     console.log('MONGO_URI', host);
     console.error(err);
